9da7b1795b1825b5dc885eadb8b922eb19cbcd14
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2020-06-14 13:36:20 -0300

    Adiciona navegacao e detalhe

--- src/main/java/com/alexandrecampos/cadastrodefuncionarios/business/bean/FuncionariosBean.java
+++ src/main/java/com/alexandrecampos/cadastrodefuncionarios/business/bean/FuncionariosBean.java
@@ -10,20 +10,33 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean(name = "funcionarios")
 public class FuncionariosBean {
 	private List<FuncionarioBean> funcionarios;
-	
+	private FuncionarioBean funcionarioSelecionado;
+
 	public FuncionariosBean() {
 		funcionarios = new ArrayList<>();
+		funcionarioSelecionado = new FuncionarioBean();
 	}
 
 	public List<FuncionarioBean> getFuncionarios() {
 		return funcionarios;
 	}
-	
+
 	public void adicionar(FuncionarioBean funcionario) {
 		funcionarios.add(funcionario);
 	}
-	
+
 	public void remover(FuncionarioBean funcionario) {
 		funcionarios.remove(funcionario);
+	}
+
+	public String selecionar(FuncionarioBean funcionario) {
+		funcionarioSelecionado = funcionario;
+		
+		// Redireciona para a pagina de detalhe quando o usuario clicar no nome
+		return "detalhe";
+	}
+
+	public FuncionarioBean getFuncionarioSelecionado() {
+		return funcionarioSelecionado;
 	}
 }
